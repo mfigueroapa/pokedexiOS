1f54a6fc98dc97ecc3014768b6f30868accdb8ac
mauricio <Mauricio@Mauricios-MacBook-Pro.local>
2018-04-07 14:25:30 -0500

    Filling CollectionView with names gotten from the API but Model organization stills undone

--- Pokedex/Controllers/PokeCollectionCellViewController.swift
+++ Pokedex/Controllers/PokeCollectionCellViewController.swift
@@ -8,44 +8,52 @@
 
 import UIKit
 
-class PokeCollectionCellViewController: UIViewController {
+class PokeCollectionCellViewController: UIViewController, UICollectionViewDataSource {
     
     @IBOutlet weak var collectionView: UICollectionView!
-    var dex = [Dex]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        Model.initialize()
+        loadJSONPokeNames(uri:"https://pokeapi.co/api/v2/pokemon/?limit=995") { dex in
+            print("success")
+            self.collectionView.dataSource = self
+        }
         print("main vc loaded")
-        
+    
 
-        let jsonUrlString = "https://pokeapi.co/api/v2/pokemon/?limit=995"
-        guard let url = URL(string:jsonUrlString) else {return}
         
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Model.dex[0].results!.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pokecell", for: indexPath) as! PokeCell
+        cell.nameLabel.text = Model.dex[0].results![indexPath.row].name!
+        return cell
+    }
+    
+    func loadJSONPokeNames(uri: String, completion: @escaping ([Dex]) -> ()) {
+        let jsonUrlString = uri
+        guard let url = URL(string:jsonUrlString) else {return}
         URLSession.shared.dataTask(with: url) { (data, response, err) in
             guard let data = data else {return}
             print("imprimiendo data: ",data)
-                
+            
             do {
-                self.dex = [try JSONDecoder().decode(Dex.self, from: data)]
+                Model.dex = [try JSONDecoder().decode(Dex.self, from: data)]
             }//end do
             catch let jsonErr {
                 print("Error serializing json: ", jsonErr)
             }//end catch
             DispatchQueue.main.async {
-                print(self.dex.count)
-                print(self.dex[0].results![23].name!)
+                completion(Model.dex)
+                //                print(self.dex.count)
+                //                print(self.dex[0].results![23].name!)
+//                self.collectionView.reloadData()
             }
         }.resume()
-        
-        
-       
-
-
-        
-        
-
-        
     }
 
 }
--- Pokedex/Model/Model.swift
+++ Pokedex/Model/Model.swift
@@ -9,59 +9,10 @@
 import UIKit
 
 class Model {
-    static var allPokemon : Array<Pokemon> = [] //Array de tipo Pokemon que esta vacio.
+    static var dex : Array<Dex> = []
     
     static func initialize() {
         print("Model initialized")
-//        Model.getJSONDataEveryPokemon() { allPokemon in //Aseguramos que el valor de allPokemon sea actualizado/regresado una vez que la descarga de datos termino.
-//            print("Primer intento de print", allPokemon[24].name!)
-//        }
-    }
-    
 
-    
-    
-//    static func getJSONDataEveryPokemon(completion: @escaping ([Pokemon]) -> ()) {
-//        let jsonUrlString = "https://pokeapi.co/api/v2/pokemon/?limit=995" //En esta url se encuentran todos los pokemon en un arreglo, y cada uno tiene dos valores asociados: nombre y url (en url se encuentra toda la demas informacion como su imagen, etc.)
-//        guard let url = URL(string:jsonUrlString) else {return}
-//        print("Print de jsonUrlString: ", jsonUrlString)
-//        print("Print de url: ", url)
-//
-//        URLSession.shared.dataTask(with: url) { (data, response, err) in
-//            guard let data = data else {return}
-//            print("imprimiendo data: ",data)
-//
-//            do {
-//                let decodedData = try JSONDecoder().decode(Dex.self, from: data)
-//                //Pokemon Objects print just for the lols
-//                print("Print de decodedData: ",decodedData.results![9].name!)
-//                print("Print de decodedData: ",decodedData.results![9].url!)
-//
-//                //Declaramos las variables necesarias para poder iterar por decodedData y
-//                //llenar el arreglo allPokemon con objetos del tipo de la clase Pokemon
-//                var i : Int = 0
-//                var pokemon : Pokemon
-//                var name : String
-//                var uri : String
-//                numberOfPokemon = decodedData.results!.count
-//                //Se crea un objeto Pokemon con los datos obtenidos y se agrega al arreglo para posteriormente repetir el proceso con un while hasta haber iterado por todos los pokemon obtenidos del api.
-//                while(i<decodedData.results!.count) { //ciclo se repite mientras nuestra variable i sea menor al tama;o de la cantidad de elementos obtenidos en results (estos son la cantidad de pokemon)
-//                    name = decodedData.results![i].name! //obtenemos el valor nombre en string de la posicion en la que estamos actualmente y lo guardamos en name
-//                    uri = decodedData.results![i].url! // obtenemos el valor url en string de la posicion en la que estamos actualmente y lo guardamos en uri
-//                    pokemon = Pokemon(name, uri, "noImage") //creamos un objeto pokemon de tipo Pokemon y le damos como parametros de entrada los valores obtenidos previamente
-//                    allPokemon.append(pokemon) // agregamos este objeto creado al arreglo de obtejos
-//                    i = i + 1 //aumentamos el contador para seguir con el siguiente
-//                    print(pokemon.name!)
-//                    print(pokemon.url!)
-//                    print("-------")
-//                    print("Numero de poke: \(numberOfPokemon)")
-//                }//end while
-//                completion(allPokemon) //Con completion vamos a avisar cuando el data task asincro (todos los datos json obtenidos) haya terminado de modo que que regresamos el valor allPokemon cuando este ya este lleno debido a que los datos ya se han terminado de bajar. De este aseguramos regresar el arreglo completo unicamente cuando se obtuvieron TODOS los valores.
-//            }//end do
-//            catch let jsonErr {
-//                print("Error serializing json: ", jsonErr)
-//            }//end catch
-//        }.resume()//After your app calls resume() on the task, it begins downloading the specified resource. After you create the task, you must start it by calling its resume() method.
-//        print("Done with getJSONDATAEveryPokemon")
-//    }//end getJSONDATAEveryPokemon
+    }
 }
